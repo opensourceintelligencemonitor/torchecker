f2df7beaf96e9abd505d918d27a7117c93cef599
Kelly Hart <opensourceintelligencemonitor@zoho.com>
2016-02-16 15:02:17 +1100

    Delete config.sh

--- config.sh
+++
@@ -1,7 +0,0 @@
-#########change the below to relate to your install##############
-WRKLOC="/home/XXXX/XXXX/torcheck"
-DB="/home/XXXX/XXXX/tordatabase.db"
-EXIT="/home/XXXX/XXXX/2hs62s72.txt"
-###################dont touch################################
-EXITDL="https://check.torproject.org/exit-addresses"
-IPCHK="http://ipecho.net/plain"
