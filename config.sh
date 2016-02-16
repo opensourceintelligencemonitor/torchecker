08d23ca35bf6f2296c3499328ea336fc3e0bc223
Kelly Hart <opensourceintelligencemonitor@zoho.com>
2016-02-16 14:31:00 +1100

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,7 @@
+#########change the below to relate to your install##############
+WRKLOC="/home/XXXX/XXXX/torcheck"
+DB="/home/XXXX/XXXX/tordatabase.db"
+EXIT="/home/XXXX/XXXX/2hs62s72.txt"
+###################dont touch################################
+EXITDL="https://check.torproject.org/exit-addresses"
+IPCHK="http://ipecho.net/plain"
